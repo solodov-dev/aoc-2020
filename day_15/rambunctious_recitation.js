230b97ec1957a1e0b1f592b69331a6558a23639b
Andrey Solodov <solodov.dev@gmail.com>
2020-12-15 17:14:55 +0300

    Day 15 OOP

--- day_15/rambunctious_recitation.js
+++ day_15/rambunctious_recitation.js
@@ -1,3 +1,5 @@
+// Functional
+
 const play = (input, limit) => {
   const history = input.reduce(
     (map, cur, index) => map.set(cur, { lastSpoken: index }),
@@ -6,26 +8,65 @@ const play = (input, limit) => {
 
   let lastNum = input[input.length - 1];
 
-  for (let i = history.size; i < limit; i++) {
+  for (let turn = history.size; turn < limit; turn++) {
     if (history.get(lastNum).beforeLastSpoken !== undefined) {
       let nextNumber = next(history.get(lastNum));
-      lastNum = update(history, nextNumber, i);
+      lastNum = update(history, nextNumber, turn);
     } else {
-      lastNum = update(history, 0, i);
+      lastNum = update(history, 0, turn);
     }
   }
 
   return lastNum;
 };
 
-const update = (map, num, last) => {
+const update = (map, num, turn) => {
   map.set(num, {
     beforeLastSpoken: map.has(num) ? map.get(num).lastSpoken : undefined,
-    lastSpoken: last,
+    lastSpoken: turn,
   });
   return num;
 };
 
 const next = (entry) => entry.lastSpoken - entry.beforeLastSpoken;
 
-export { play };
+// OOP
+class Game {
+  constructor(input, limit) {
+    this.limit = limit;
+    this.history = input.reduce(
+      (map, cur, index) => map.set(cur, { lastSpoken: index }),
+      new Map()
+    );
+    this.lastNum = input[input.length - 1];
+  }
+
+  play() {
+    for (let turn = this.history.size; turn < this.limit; turn++) {
+      this.last.beforeLastSpoken !== undefined
+        ? (this.lastNum = this.update(this.next, turn))
+        : (this.lastNum = this.update(0, turn));
+    }
+    return this.lastNum;
+  }
+
+  get next() {
+    return this.last.lastSpoken - this.last.beforeLastSpoken;
+  }
+
+  get last() {
+    return this.history.get(this.lastNum);
+  }
+
+  update(num, turn) {
+    this.history.set(num, {
+      beforeLastSpoken: this.history.has(num)
+        ? this.history.get(num).lastSpoken
+        : undefined,
+      lastSpoken: turn,
+    });
+    return num;
+  }
+}
+
+export { play, Game };
--- day_15/rambunctious_recitation.test.js
+++ day_15/rambunctious_recitation.test.js
@@ -1,6 +1,6 @@
-import { play } from './rambunctious_recitation.js';
+import { Game, play } from './rambunctious_recitation.js';
 
-describe('Rambunctions recitation', () => {
+describe('Rambunctions recitation functional', () => {
   it('Plays the game', () => {
     expect(play([0, 3, 6], 10)).toBe(0);
   });
@@ -24,5 +24,20 @@ describe('Rambunctions recitation', () => {
   });
   it('Plays the game', () => {
     expect(play([3, 1, 2], 2020)).toBe(1836);
+  });
+});
+
+describe('Rambunctions recitation OOP', () => {
+  it('Plays the game', () => {
+    const game = new Game([0, 3, 6], 10);
+    expect(game.play()).toBe(0);
+  });
+  it('Plays the game', () => {
+    const game = new Game([0, 3, 6], 2020);
+    expect(game.play()).toBe(436);
+  });
+  it('Plays the game', () => {
+    const game = new Game([1, 3, 2], 2020);
+    expect(game.play()).toBe(1);
   });
 });
